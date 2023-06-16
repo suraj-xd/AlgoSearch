e87937bfd4064b7d831ddda9343687acba153ced
Suraj <unofficialartist3@gmail.com>
2023-06-16 20:51:13 +0530

    new cjhagnes

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
-module.exports = {
-  images: {
-    domains: [
-      "links.papareact.com",
-      "platform-lookaside.fbsbx.com",
-      "firebasestorage.googleapis.com",
-      "lh3.googleusercontent.com"
-    ],
-  },
-  webpack: function (config, options) {
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true,
-      topLevelAwait: true,
-    };
-    return config;
-  },
-  optimizeFonts: false
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
